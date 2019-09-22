62f2861bfeb16f5225075167106946ceabb81116
James Ward <jpcmward@gmail.com>
2019-09-22 18:18:20 +0100

    Added Main and MainUI classes.

---
+++ src/jwrc/main/Main.java
@@ -0,0 +1,10 @@
+package jwrc.main;
+
+import jwrc.ui.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainUI mainUI = new MainUI();
+    }
+}
---
+++ src/jwrc/ui/MainUI.java
@@ -0,0 +1,13 @@
+package jwrc.ui;
+
+import javax.swing.*;
+
+public class MainUI extends JFrame {
+
+    public MainUI() {
+        super("Monopoly");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1200, 800);
+        this.setVisible(true);
+    }
+}
