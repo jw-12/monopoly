4ea049d07dfe4488b762bcc87bd373a02bd794ef
Ronan Corcoran <ronan.corcoran.1@ucdconnect.ie>
2019-10-02 00:05:43 +0100

    updated board array

--- src/jwrc/board/Board.java
+++ src/jwrc/board/Board.java
@@ -6,29 +6,33 @@ public class Board {
 
    // ArrayList<BoardSpace> boardSpaces;
 	
-	ArrayList<BoardSpace> propertyBoard;  // just for testing 
+	//ArrayList<BoardSpace> propertyBoard;  // just for testing 
+	BoardSpace[] propertyBoard;
     
     public Board() {
-    	this.propertyBoard = new ArrayList<BoardSpace>();
+    	//this.propertyBoard = new ArrayList<BoardSpace>();
+    	this.propertyBoard = new BoardSpace[5];
     }
     
     
     public void setBoard() { // will think of more efficient way to create board.
-    	BoardSpace bs1 = new Property("Mediterranean Avenue",60 ,"Brown",1);
-    	propertyBoard.add(bs1);
-    	BoardSpace bs2 = new Property("Baltic Avenue",60,"Brown",3);
-    	propertyBoard.add(bs2);
-    	BoardSpace bs3 = new Property("Oriental Avenue",100,"LightBlue",6);
-    	propertyBoard.add(bs3);
-    	BoardSpace bs4 = new Property("Vermont Avenue",100,"LightBlue",8);
-    	propertyBoard.add(bs4);
-    	BoardSpace bs5 = new Property("Connecticut Avenue",120,"LightBlue",9);
-    	propertyBoard.add(bs5);
+    	propertyBoard[0] = new Property("Mediterranean Avenue",60 ,"Brown",1);
+    	
+    	propertyBoard[1] = new Property("Baltic Avenue",60,"Brown",3);
+    
+    	propertyBoard[2] = new Property("Oriental Avenue",100,"LightBlue",6);
+    
+    	propertyBoard[3] = new Property("Vermont Avenue",100,"LightBlue",8);
+    
+    	propertyBoard[4] = new Property("Connecticut Avenue",120,"LightBlue",9);
+    
     	
     	//test
-    	for(int i=0; i<5; i++) {
-    		System.out.println(((Property)propertyBoard.get(i)).getName() +" is at board index"+ ((Property)propertyBoard.get(i)).getBoardIndex());
-    	}
+    //	for(int i=0; i<5; i++) {
+    //		System.out.println(((Property)propertyBoard.get(i)).getName() +" is at board index "+ ((Property)propertyBoard.get(i)).getBoardIndex());
+    //	}
+    	
+    	System.out.println(((Property)propertyBoard[0]).getName());
     	
     }
     
