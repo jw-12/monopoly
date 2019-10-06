039c3c0beed3e58f01c0da91d3e1d4f68a9e23a3
James Ward <jpcmward@gmail.com>
2019-10-06 23:21:35 +0100

    Main reverted from "testing auction mode"

--- src/jwrc/main/Main.java
+++ src/jwrc/main/Main.java
@@ -1,7 +1,6 @@
 package jwrc.main;
 
 import jwrc.board.Board;
-import jwrc.game.Auction;
 import jwrc.game.Game;
 
 import java.util.Scanner;
@@ -12,16 +11,14 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        Auction.testAuction();
-
-        /*Game game = new Game(input);
+        Game game = new Game(input);
         game.preGame();
         game.start();
         
         //Board newBoard = new Board();
         //newBoard.setBoard();
 
-        input.close();*/
+        input.close();
 
     }
 }
